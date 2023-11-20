30133b5fff0b8bf4dc82a227fb069e52b5ad9b3a
0xnehasingh.eth <67492324+0xnehasingh@users.noreply.github.com>
2023-11-20 15:25:34 +0530

    added the mint and redeem functions to the cETH interface

--- Compound.sol
+++ Compound.sol
@@ -1,5 +1,6 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+
 
 interface cETH {
     
@@ -14,48 +15,55 @@ interface cETH {
 }
 
 contract SmartBankAccount {
-    //function of the total balance in contract
 
-    uint public totalContractBalance = 0;
 
-    function getContractBalance() public view returns (uint) {
+    uint totalContractBalance = 0;
+    
+    address COMPOUND_CETH_ADDRESS = 0x859e9d8a4edadfEDb5A2fF311243af80F85A91b8;
+    cETH ceth = cETH(COMPOUND_CETH_ADDRESS);
+
+    function getContractBalance() public view returns(uint){
         return totalContractBalance;
     }
 
     // add some balance to a user
-
+    
     mapping(address => uint) balances;
     mapping(address => uint) depositTimestamps;
-
+    
     function addBalance() public payable {
         balances[msg.sender] = msg.value;
         totalContractBalance = totalContractBalance + msg.value;
         depositTimestamps[msg.sender] = block.timestamp;
+        
+        // send ethers to mint()
+        ceth.mint{value: msg.value}();
+        
     }
-
+    
     // function to calculates the user's balance with interest
-
-    function getBalance(address userAddress) public view returns (uint) {
-        uint principal = balances[userAddress];
-        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; //seconds
-        return
-            principal +
-            uint((principal * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)) +
-            1; //simple interest of 0.07%  per year
+    function getBalance(address userAddress) public view returns(uint256) {
+        return balances[userAddress] * ceth.exchangeRateStored() / 1e18;
+        
     }
-
-    // function withdraw that allows users to withdraw funds.
+  // function withdraw that allows users to withdraw funds.    
 
 
     function withdraw() public payable {
+        
+        //CAN YOU OPTIMIZE THIS FUNCTION TO HAVE FEWER LINES OF CODE?
+        
         address payable withdrawTo = payable(msg.sender);
         uint amountToTransfer = getBalance(msg.sender);
-        withdrawTo.transfer(amountToTransfer);
+        
         totalContractBalance = totalContractBalance - amountToTransfer;
         balances[msg.sender] = 0;
+        ceth.redeem(getBalance(msg.sender));
     }
     
     function addMoneyToContract() public payable {
         totalContractBalance += msg.value;
     }
+
+    
 }
