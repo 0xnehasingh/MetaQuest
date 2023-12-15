f1a5d52fcc8e25ff390ca7b21e58589f78d8662b
0xnehasingh.eth <67492324+0xnehasingh@users.noreply.github.com>
2023-12-15 14:49:30 +0530

    supply money to contract

--- SmartBankAccount.sol
+++ SmartBankAccount.sol
@@ -5,8 +5,13 @@ pragma solidity ^0.8.0;
 contract SmartBankAccount {
     uint public totalContractBalance = 0;
 
-    function getContractBalance() public view returns(uint){
+    function getContractBalance() public view returns (uint) {
         return totalContractBalance;
     }
 
+    mapping(address => uint) balances;
+    function addBalance(address userAddress, uint amount) public payable {
+        balances[userAddress] = amount;
+        totalContractBalance = totalContractBalance + amount;
+    }
 }
